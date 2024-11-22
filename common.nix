8d94225e18ef246d7bb700554cc4b29dd0176f25
flyfloh <webstuff@fly-soft.de>
2024-11-22 17:47:08 +0100

    add common.nix

---
+++ common.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+    settings = {
+      sandbox = true;
+      trusted-users = [ "root" "@wheel" ];
+    };
+  };
+
+  networking.useDHCP = false;
+
+  programs = {
+    command-not-found.enable = true;
+    vim = {
+      enable = true;
+      defaultEditor = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    ripgrep
+    tmux
+    vim
+    wget
+  ];
+}
+
